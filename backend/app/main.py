eceb587c0b2fdc9b34a34b8006ee71e725c9e02e
Matheus Rogerio Pesarini <mrogeriopesarini@gmail.com>
2026-04-07 11:20:58 -0300

    feat: incluir roteador de luthier e atualizar dependências no requirements.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,11 @@ from functools import lru_cache
 from fastapi import FastAPI
 
 from app.core import config
-from app.core.database import Base, engine
+from app.routers import luthier_router
 
 app = FastAPI(title="FastAPI Example", description="A simple FastAPI application", version="1.0.0")
+
+app.include_router(luthier_router.router)
 
 @lru_cache
 def get_settings():
