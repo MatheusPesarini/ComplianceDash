22ea7c56c4228e4d1e55c6cb0e4b044ad8e1d571
Matheus Rogerio Pesarini <mrogeriopesarini@gmail.com>
2026-05-03 19:12:20 -0300

    feat: enhance user registration and login endpoints with response models and error handling

--- backend/app/routers/luthier_router.py
+++ backend/app/routers/luthier_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 from app.core.database import get_db
-from app.schemas.luthier_schema import UserCreate, UserCreateResponse, UserLogin, EquipamentCreate, EquipamentResponse, EquipamentUpdate
+from app.schemas.luthier_schema import UserCreate, UserCreateResponse, UserLogin, EquipamentCreate, EquipamentResponse, EquipamentUpdate, UserLoginResponse
 from app.services.luthier_service import UserService
 
 
@@ -11,12 +12,28 @@ router = APIRouter(prefix="/api", tags=["users"])
 @router.post("/register", response_model=UserCreateResponse, status_code=201)
 def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
     service = UserService(db)
-    return service.create_user(user_in)
-
-@router.post("/login", response_model=None, status_code=201)
+    try:
+        service.create_user(user_in)
+        return UserCreateResponse(successful=True)
+    except IntegrityError:
+        # ex: e-mail já existe [unique=True]
+        db.rollback() 
+        return UserCreateResponse(successful=False, error_message="Este email já está cadastrado")
+    except Exception as e:
+        return UserCreateResponse(successful=False, error_message=str(e))
+    
+@router.post("/login", response_model=UserLoginResponse, status_code=200)
 def login_user(user_in: UserLogin, db: Session = Depends(get_db)):
     service = UserService(db)
-    return service.login_user(user_in)
+    try:
+        service.login_user(user_in) 
+        return UserLoginResponse(successful=True)
+    except ValueError as e:
+        # ValueError = "Credenciais inválidas"
+        return UserLoginResponse(successful=False, error_message=str(e))
+    except Exception as e:
+        return UserLoginResponse(successful=False, error_message=str(e))
+
 
 @router.get("/user/{user_id}", response_model=UserCreateResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
