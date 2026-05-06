f171a0267b2622cf3b688402abdbe542a5e9583a
Matheus Rogerio Pesarini <mrogeriopesarini@gmail.com>
2026-05-06 19:33:24 -0300

    fix: update token expiration handling to use UTC and improve error handling in get_current_user function

--- backend/app/core/token_jwt.py
+++ backend/app/core/token_jwt.py
@@ -1,6 +1,7 @@
 import jwt
 from app.core.config import settings
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
+from fastapi import HTTPException
 
 secret_key = settings.secret_key
 algorithm = "HS256"
@@ -9,7 +10,7 @@ access_token_expire_minutes = 60
 def create_acess_token(data: dict):
     to_encode = data.copy()
     
-    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=access_token_expire_minutes)
+    expire = datetime.now(UTC) + timedelta(minutes=access_token_expire_minutes)
     to_encode.update({"exp": expire})
     
     encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
@@ -18,3 +19,15 @@ def create_acess_token(data: dict):
 def verify_token(token: str):
     payload = jwt.decode(token, secret_key, algorithms=[algorithm])
     return payload
+
+def get_current_user(token: str):
+    try:
+        payload = verify_token(token)
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Token inválido")
+        return {"id": user_id}
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expirado")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token inválido")
